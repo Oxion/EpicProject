05392c60e393950bb2fded4d2be51f217dfb066c
Kazes <kazes451@gmail.com>
2016-10-05 23:58:32 +0400

    добавил .tmp в игнор

--- learnScript/helloworld.js
+++ learnScript/helloworld.js
@@ -1,12 +1,14 @@
 const http = require('http');
+const os = require('os');
 
 const hostname ='0.0.0.0';
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(JSON.stringify(os.cpus()));
 });
 
 server.listen(port, hostname, () => {
